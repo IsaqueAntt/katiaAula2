cf9bc1a31f1a27195bfe6c0f967b2213f769f1e9
curso.ads <curso.ads@LAB06-10>
2022-08-22 22:26:12 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaraçao e instanciaçao do objeto
+            Produto prod = new Produto();
+            Cliente cli = new Cliente();
+            Console.Write("Digite o nome do cliente");
+            cli.setNome(Console.ReadLine());
+
+            Console.Write("Digite o CPF do cliente");
+            cli.setCpf(Console.ReadLine());
+
+            Console.Write("Digite o endereço do cliente");
+            cli.setEndereco(Console.ReadLine());
+
+            Console.Write("digite a idade do cliente");
+            cli.setIdade(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Digite o codigo do produto: ");
+            prod.setCodigo(Convert.ToInt32(Console.ReadLine()));
+
+            Console.Write("Digite a descricao do produto:");
+            prod.setDescricao(Console.ReadLine());
+
+            Console.Write("Digite a categoria do produto: ");
+            prod.setCategoria(Console.ReadLine());
+
+            Console.Write("Digite o valor do produto: ");
+            prod.setValor(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Codigo = " + prod.getCodigo() + "\nDescricao:" + prod.getDescricao() + "\nCategoria: = " + prod.getCategoria() + "\nValor = " + prod.getValor().ToString());
+
+            Console.WriteLine("Nome = " + cli.getNome() + "\nEndereco = " + cli.getEndereco() + "\nCPF =" + cli.getCpf() + "\nIdade =" + cli.getIdade());
+            
+            Console.ReadKey();
+
+        }
+    }
+}
